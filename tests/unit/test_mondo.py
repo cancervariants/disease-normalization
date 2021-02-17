6b02c617c8ffb0bade04182ed6d1a6e9de320830
James Stevenson <james.stevenson@nationwidechildrens.org>
2021-02-17 14:09:52 -0500

    Update tests for mondo

---
+++ tests/unit/test_mondo.py
@@ -0,0 +1,193 @@
+"""Test MONDO ETL methods."""
+import pytest
+from disease.schemas import MatchType
+from disease.query import QueryHandler
+from typing import Dict
+
+
+@pytest.fixture(scope='module')
+def mondo():
+    """Build MONDO ETL test fixture."""
+    class QueryGetter:
+
+        def __init__(self):
+            self.query_handler = QueryHandler()
+
+        def search(self, query_str):
+            response = self.query_handler.search_sources(query_str, keyed=True,
+                                                         incl='mondo')
+            return response['source_matches']['MONDO']
+    return QueryGetter()
+
+
+@pytest.fixture(scope='module')
+def neuroblastoma():
+    """Construct a test fixture for neuroblastoma"""
+    return {
+        "concept_id": "mondo:0005072",
+        "label": "neuroblastoma",
+        "aliases": [
+            "neural Crest tumor, malignant",
+            "neuroblastoma (Schwannian Stroma-poor)",
+            "neuroblastoma, malignant"
+        ],
+        "other_identifiers": ["ncit:C3270"],
+        "xrefs": [
+            "orphanet:635",
+            "nifstd:birnlex_12631",
+            "umls:C0027819",
+            "DOID:769",
+            "gard:0007185",
+            "meddra:10029260",
+            "icdo:9500/3",
+            "sctid:432328008",
+            "umls:CN205405",
+            "efo:0000621",
+            "icd10:C74.9",
+            "oncotree:NBL",
+            "mesh:D009447"
+        ]
+    }
+
+
+@pytest.fixture(scope='module')
+def richter_syndrome():
+    """Construct a test fixture for Richter Syndrome"""
+    return {
+        "concept_id": "mondo:0002083",
+        "label": "Richter syndrome",
+        "aliases": [
+            "Richter's syndrome",
+            "Richter transformation",
+            "Richter's transformation"
+        ],
+        "other_identifiers": ["ncit:C35424"],
+        "xrefs": [
+            "icd10:C91.1",
+            "sctid:277550009",
+            "umls:C0349631",
+            "gard:0007578",
+            "DOID:1703"
+        ]
+    }
+
+
+def compare_records(actual_record: Dict, fixture_record: Dict):
+    """Check that identity records are identical."""
+    assert actual_record['concept_id'] == fixture_record['concept_id']
+    assert ('label' in actual_record) == ('label' in fixture_record)
+    if 'label' in actual_record or 'label' in fixture_record:
+        assert actual_record['label'] == fixture_record['label']
+    assert ('aliases' in actual_record) == ('aliases' in fixture_record)
+    if 'aliases' in actual_record or 'aliases' in fixture_record:
+        assert set(actual_record['aliases']) == set(fixture_record['aliases'])
+    assert ('other_identifiers' in actual_record) == ('other_identifiers' in fixture_record)  # noqa: E501
+    if 'other_identifiers' in actual_record or 'other_identifiers' in fixture_record:  # noqa: E501
+        assert set(actual_record['other_identifiers']) == set(fixture_record['other_identifiers'])  # noqa: E501
+    assert ('xrefs' in actual_record) == ('xrefs' in fixture_record)
+    if 'xrefs' in actual_record or 'xrefs' in fixture_record:
+        assert set(actual_record['xrefs']) == set(fixture_record['xrefs'])
+
+
+def test_concept_id_match(ncit, neuroblastoma, richter_syndrome):
+    """Test that concept ID search resolves to correct record"""
+    response = ncit.search('mondo:0005072')
+    assert response['match_type'] == MatchType.CONCEPT_ID
+    assert len(response['records']) == 1
+    actual_disease = response['records'][0].dict()
+    compare_records(actual_disease, neuroblastoma)
+
+    response = ncit.search('mondo:0002083')
+    assert response['match_type'] == MatchType.CONCEPT_ID
+    assert len(response['records']) == 1
+    actual_disease = response['records'][0].dict()
+    compare_records(actual_disease, richter_syndrome)
+
+    response = ncit.search('MONDO:0005072')
+    assert response['match_type'] == MatchType.CONCEPT_ID
+    assert len(response['records']) == 1
+    actual_disease = response['records'][0].dict()
+    compare_records(actual_disease, richter_syndrome)
+
+    response = ncit.search('0002083')
+    assert response['match_type'] == MatchType.NO_MATCH
+
+
+def test_label_match(ncit, neuroblastoma, richter_syndrome):
+    """Test that label search resolves to correct record."""
+    response = ncit.search('Neuroblastoma')
+    assert response['match_type'] == MatchType.LABEL
+    assert len(response['records']) == 1
+    actual_disease = response['records'][0].dict()
+    compare_records(actual_disease, neuroblastoma)
+
+    response = ncit.search('NEUROBLASTOMA')
+    assert response['match_type'] == MatchType.LABEL
+    assert len(response['records']) == 1
+    actual_disease = response['records'][0].dict()
+    compare_records(actual_disease, neuroblastoma)
+
+    response = ncit.search('richter syndrome')
+    assert response['match_type'] == MatchType.LABEL
+    assert len(response['records']) == 1
+    actual_disease = response['records'][0].dict()
+    compare_records(actual_disease, richter_syndrome)
+
+
+def test_alias_match(ncit, neuroblastoma, richter_syndrome):
+    """Test that alias search resolves to correct record."""
+    response = ncit.search('neuroblastoma, malignant')
+    assert response['match_type'] == MatchType.ALIAS
+    assert len(response['records']) == 1
+    actual_disease = response['records'][0].dict()
+    compare_records(actual_disease, neuroblastoma)
+
+    response = ncit.search('RICHTER TRANSFORMATION')
+    assert response['match_type'] == MatchType.ALIAS
+    assert len(response['records']) == 1
+    actual_disease = response['records'][0].dict()
+    compare_records(actual_disease, richter_syndrome)
+
+    response = ncit.search('Neuroblastoma, Malignant')
+    assert response['match_type'] == MatchType.ALIAS
+    assert len(response['records']) == 1
+    actual_disease = response['records'][0].dict()
+    compare_records(actual_disease, neuroblastoma)
+
+    response = ncit.search('neuroblastoma (Schwannian Stroma-poor)')
+    assert response['match_type'] == MatchType.ALIAS
+    assert len(response['records']) == 1
+    actual_disease = response['records'][0].dict()
+    compare_records(actual_disease, neuroblastoma)
+
+    response = ncit.search("Richter's transformation")
+    assert response['match_type'] == MatchType.ALIAS
+    assert len(response['records']) == 1
+    actual_disease = response['records'][0].dict()
+    compare_records(actual_disease, richter_syndrome)
+
+    response = ncit.search("Richter's syndrome")
+    assert response['match_type'] == MatchType.ALIAS
+    assert len(response['records']) == 1
+    actual_disease = response['records'][0].dict()
+    compare_records(actual_disease, richter_syndrome)
+
+    response = ncit.search("neuroblastoma Schwannian Stroma-poor")
+    assert response['match_type'] == MatchType.NO_MATCH
+
+
+def test_meta(ncit):
+    """Test that meta field is correct."""
+    response = ncit.search('neuroblastoma')
+    assert response['meta_'].data_license == 'CC BY 4.0'
+    assert response['meta_'].data_license_url == \
+        'https://creativecommons.org/licenses/by/4.0/legalcode'
+    assert response['meta_'].version == '20210129'
+    assert response['meta_'].data_url == \
+        'https://mondo.monarchinitiative.org/pages/download/'
+    assert response['meta_'].rdp_url == 'http://reusabledata.org/monarch.html'
+    assert response['meta_'].data_license_attributes == {
+        "non_commercial": False,
+        "share_alike": False,
+        "attribution": True
+    }
