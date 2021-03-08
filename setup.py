3094aa799db53825b8325e2eee97c345aff5b00e
James Stevenson <james.stevenson@nationwidechildrens.org>
2021-03-08 10:02:29 -0500

    Remove version constant

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
 """Defines packaging and distribution."""
 from setuptools import setup, find_packages
-from disease import __version__
 
-setup(version=__version__,
-      packages=find_packages())
+setup(packages=find_packages())
