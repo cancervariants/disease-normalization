0e43f7e8e14140ccf063f62adeb5e9f1aad39404
James Stevenson <james.stevenson@nationwidechildrens.org>
2021-10-07 09:22:14 -0400

    Clean up log initialization

--- disease/__init__.py
+++ disease/__init__.py
@@ -1,18 +1,11 @@
 """The VICC library for normalizing diseases."""
+from .version import __version__  # noqa: F401
 from pathlib import Path
 import logging
 from os import environ
 
-from .version import __version__  # noqa: F401
-
 
 PROJECT_ROOT = Path(__file__).resolve().parents[0]
-
-logging.basicConfig(
-    filename='disease.log',
-    format='[%(asctime)s] - %(name)s - %(levelname)s : %(message)s')
-logger = logging.getLogger('disease')
-logger.setLevel(logging.DEBUG)
 
 # establish environment-dependent params
 if "DISEASE_NORM_EB_PROD" in environ:
@@ -21,6 +14,7 @@ if "DISEASE_NORM_EB_PROD" in environ:
 else:
     LOG_FN = "disease.log"
 
+# set up logging
 logging.basicConfig(
     filename=LOG_FN,
     format="[%(asctime)s] - %(name)s - %(levelname)s : %(message)s"
