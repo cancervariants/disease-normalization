36e9c3b9f1f14728b7bfc05b05ab64e02e880ea0
James Stevenson <james.stevenson@nationwidechildrens.org>
2023-04-21 14:53:29 -0400

    fix fixture

--- tests/unit/test_database.py
+++ tests/unit/test_database.py
@@ -26,36 +26,36 @@ IS_DDB = not os.environ.get("DISEASE_NORM_DB_URL", "").lower().startswith("postg
 
 
 @pytest.mark.skipif(not IS_DDB, reason="only applies to DynamoDB in test env")
-def test_item_type(db):
+def test_item_type(database):
     """Check that objects are tagged with item_type attribute."""
     from boto3.dynamodb.conditions import Key
 
     filter_exp = Key("label_and_type").eq("ncit:c2926##identity")
-    item = db.diseases.query(KeyConditionExpression=filter_exp)["Items"][0]
+    item = database.diseases.query(KeyConditionExpression=filter_exp)["Items"][0]
     assert "item_type" in item
     assert item["item_type"] == "identity"
 
     filter_exp = Key("label_and_type").eq("neuroblastoma##label")
-    item = db.diseases.query(KeyConditionExpression=filter_exp)["Items"][0]
+    item = database.diseases.query(KeyConditionExpression=filter_exp)["Items"][0]
     assert "item_type" in item
     assert item["item_type"] == "label"
 
     filter_exp = Key("label_and_type").eq("umls:c0027819##associated_with")
-    item = db.diseases.query(KeyConditionExpression=filter_exp)["Items"][0]
+    item = database.diseases.query(KeyConditionExpression=filter_exp)["Items"][0]
     assert "item_type" in item
     assert item["item_type"] == "associated_with"
 
     filter_exp = Key("label_and_type").eq("oncotree:nbl##xref")
-    item = db.diseases.query(KeyConditionExpression=filter_exp)["Items"][0]
+    item = database.diseases.query(KeyConditionExpression=filter_exp)["Items"][0]
     assert "item_type" in item
     assert item["item_type"] == "xref"
 
     filter_exp = Key("label_and_type").eq("childhood liposarcoma##alias")
-    item = db.diseases.query(KeyConditionExpression=filter_exp)["Items"][0]
+    item = database.diseases.query(KeyConditionExpression=filter_exp)["Items"][0]
     assert "item_type" in item
     assert item["item_type"] == "alias"
 
     filter_exp = Key("label_and_type").eq("ncit:c9012##merger")
-    item = db.diseases.query(KeyConditionExpression=filter_exp)["Items"][0]
+    item = database.diseases.query(KeyConditionExpression=filter_exp)["Items"][0]
     assert "item_type" in item
     assert item["item_type"] == "merger"
