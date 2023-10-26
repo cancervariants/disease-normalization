074dd51654953cffb3e6f18f9ee1fc90319cd279
James Stevenson <james.stevenson@nationwidechildrens.org>
2023-10-26 10:35:48 -0400

    feat: add get_all_records DB method (#160)

--- tests/unit/test_database.py
+++ tests/unit/test_database.py
@@ -3,6 +3,11 @@ import os
 
 import pytest
 
+from disease.schemas import RecordType
+
+IS_DDB = not os.environ.get("DISEASE_NORM_DB_URL", "").lower().startswith("postgres")
+IS_TEST_ENV = os.environ.get("DISEASE_TEST", "").lower() == "true"
+
 
 def test_tables_created(database):
     """Check that required tables are created."""
@@ -20,9 +25,6 @@ def test_tables_created(database):
     else:
         assert database.disease_concepts_table in existing_tables
         assert database.disease_metadata_table in existing_tables
-
-
-IS_DDB = not os.environ.get("DISEASE_NORM_DB_URL", "").lower().startswith("postgres")
 
 
 @pytest.mark.skipif(not IS_DDB, reason="only applies to DynamoDB in test env")
@@ -59,3 +61,22 @@ def test_item_type(database):
     item = database.diseases.query(KeyConditionExpression=filter_exp)["Items"][0]
     assert "item_type" in item
     assert item["item_type"] == "merger"
+
+
+@pytest.mark.skipif(not IS_TEST_ENV, reason="not in test environment")
+def database(db_fixture):
+    """Perform basic test of get_all_records method.
+
+    It's probably overkill (and unmaintainable) to do exact checks against every
+    record, but fairly easy to check against expected counts and ensure that nothing
+    is getting sent twice.
+    """
+    source_records = list(db_fixture.get_all_records(RecordType.IDENTITY))
+    assert len(source_records) == 1463
+    source_ids = {r["concept_id"] for r in source_records}
+    assert len(source_ids) == 1463
+
+    normalized_records = list(db_fixture.get_all_records(RecordType.MERGER))
+    assert len(normalized_records) == 1391
+    normalized_ids = {r["concept_id"] for r in normalized_records}
+    assert len(normalized_ids) == 1391
