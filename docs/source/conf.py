f12501b36556055d11b0da11a34f0221728e1451
James Stevenson <james.stevenson@nationwidechildrens.org>
2024-02-05 11:31:35 -0500

    test?

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,7 +56,7 @@ html_theme_options = {
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../../"))
+sys.path.insert(0, os.path.abspath("../../src/disease"))
 autodoc_preserve_defaults = True
 
 # -- get version -------------------------------------------------------------
