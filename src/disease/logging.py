500295ebdf479fe5f21e384a3d666de7ebac9326
James Stevenson <james.stevenson@nationwidechildrens.org>
2025-02-25 09:41:33 -0500

    feat: add service_info endpoint and update fastapi architecture

---
+++ src/disease/logging.py
@@ -0,0 +1,15 @@
+"""Configure application logging."""
+
+import logging
+
+
+def initialize_logs(log_level: int = logging.DEBUG) -> None:
+    """Configure logging.
+    :param log_level: app log level to set
+    """
+    logging.basicConfig(
+        filename=f"{__package__}.log",
+        format="[%(asctime)s] - %(name)s - %(levelname)s : %(message)s",
+    )
+    logger = logging.getLogger(__package__)
+    logger.setLevel(log_level)
--- src/disease/main.py
+++ src/disease/main.py
@@ -1,50 +1,67 @@
 """Main application for FastAPI"""
 
 import html
+from collections.abc import AsyncGenerator
+from contextlib import asynccontextmanager
+from enum import Enum
 
-from fastapi import FastAPI, HTTPException, Query
-from fastapi.openapi.utils import get_openapi
+from fastapi import FastAPI, HTTPException, Query, Request
 
 from disease import __version__
+from disease.config import config
 from disease.database.database import create_db
+from disease.logging import initialize_logs
 from disease.query import InvalidParameterException, QueryHandler
-from disease.schemas import NormalizationService, SearchService
+from disease.schemas import (
+    APP_DESCRIPTION,
+    GITHUB_REPO_URL,
+    LAB_EMAIL,
+    LAB_WEBPAGE_URL,
+    NormalizationService,
+    SearchService,
+    ServiceInfo,
+    ServiceOrganization,
+    ServiceType,
+)
 
-db = create_db()
-query_handler = QueryHandler(db)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator:  # noqa: ARG001
+    """Perform operations that interact with the lifespan of the FastAPI instance.
+    See https://fastapi.tiangolo.com/advanced/events/#lifespan.
+    :param app: FastAPI instance
+    """
+    initialize_logs()
+    db = create_db()
+    query_handler = QueryHandler(db)
+    yield {"query_handler": query_handler}
+    db.close_connection()
+
 
 app = FastAPI(
+    title="VICC Disease Normalizer",
+    description=APP_DESCRIPTION,
+    version=__version__,
+    contact={
+        "name": "Alex H. Wagner",
+        "email": LAB_EMAIL,
+        "url": LAB_WEBPAGE_URL,
+    },
+    license={
+        "name": "MIT",
+        "url": f"{GITHUB_REPO_URL}/blob/main/LICENSE",
+    },
     docs_url="/disease",
     openapi_url="/disease/openapi.json",
     swagger_ui_parameters={"tryItOutEnabled": True},
 )
 
 
-def custom_openapi() -> dict | None:
-    """Generate custom fields for OpenAPI response"""
-    if app.openapi_schema:
-        return app.openapi_schema
-    openapi_schema = get_openapi(
-        title="The VICC Disease Normalizer",
-        version=__version__,
-        openapi_version="3.0.3",
-        description="Normalize disease terms.",
-        routes=app.routes,
-    )
-    #    openapi_schema['info']['license'] = {  # TODO
-    #        "name": "Name-of-license",
-    #        "url": "http://www.to-be-determined.com"
-    #    }
-    openapi_schema["info"]["contact"] = {
-        "name": "Alex H. Wagner",
-        "email": "Alex.Wagner@nationwidechildrens.org",
-        "url": "https://www.nationwidechildrens.org/specialties/institute-for-genomic-medicine/research-labs/wagner-lab",
-    }
-    app.openapi_schema = openapi_schema
-    return app.openapi_schema
+class _Tag(str, Enum):
+    """Define tag names for endpoints."""
 
+    META = "Meta"
 
-app.openapi = custom_openapi
 
 # endpoint description text
 get_matches_summary = "Given query, provide highest matches from " "each source."
@@ -78,6 +95,7 @@ normalize_description = (
     response_model_exclude_none=True,
 )
 def search(
+    request: Request,
     q: str = Query(..., description=q_descr),
     incl: str | None = Query("", description=incl_descr),
     excl: str | None = Query("", description=excl_descr),
@@ -90,6 +108,7 @@ def search(
     :param excl: sources to excl
     :return: search results
     """
+    query_handler = request.state.query_handler
     try:
         response = query_handler.search(html.unescape(q), incl=incl, excl=excl)
     except InvalidParameterException as e:
@@ -111,13 +130,32 @@ merged_q_descr = "Disease to normalize."
     description=normalize_description,
     response_model_exclude_none=True,
 )
-def normalize(q: str = Query(..., description=merged_q_descr)) -> NormalizationService:
+def normalize(
+    request: Request, q: str = Query(..., description=merged_q_descr)
+) -> NormalizationService:
     """Return strongest-match normalized concept for query string provided by
     user.
+
     :param q: disease search term
     """
+    query_handler = request.state.query_handler
     try:
         response = query_handler.normalize(q)
     except InvalidParameterException as e:
         raise HTTPException(status_code=422, detail=str(e)) from None
     return response
+
+
+@app.get(
+    "/service_info",
+    summary="Get basic service information",
+    description="Retrieve service metadata, such as versioning and contact info. Structured in conformance with the [GA4GH service info API specification](https://www.ga4gh.org/product/service-info/)",
+    tags=[_Tag.META],
+)
+def service_info() -> ServiceInfo:
+    """Provide service info per GA4GH Service Info spec
+    :return: conformant service info description
+    """
+    return ServiceInfo(
+        organization=ServiceOrganization(), type=ServiceType(), environment=config.env
+    )
